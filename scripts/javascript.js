69a40c15b4834ba85766e718575c717f7829cf93
EdouardPascal <pascaledouard1@gmail.com>
2023-05-10 23:15:43 -0400

    Made the index page responsive using javascrip once you click on a button it changes the css file to dark mode

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,30 @@
 console.log("My name is Pascal");
 let name='Pascal';
+var nightmode=document.getElementById("darkmode");
+nightmode.addEventListener('click',switch_dark);
+var bouton=document.getElementById("btn");
 
+
+function switch_dark()
+{
+    var style=document.getElementById("principal");
+    var picture=document.querySelector("img#darkmode");
+    if(style.href=="style/dark.css")
+    {
+        style.href="style/style.css";
+        picture.src="img/night-mode.png";
+    }
+    else
+    {
+        style.href="style/dark.css";
+        picture.src="img/dark-mode.png";
+        
+    }
+   
+}
+bouton.addEventListener('click',clicked);
+function clicked()
+{
+    alert("You haved clicked on the button");
+}
 alert(name);
